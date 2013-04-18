9364672671ab49bfcc8f2180bd1086ba6ef10b30
Igor Starodynov <igor.starodynov@outlook.com>
2013-04-19 00:12:58 +0300

    Added some resources.

---
+++ StenoCryptor.Commons/CryptType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StenoCryptor.Commons
+{
+    public enum CryptType: byte
+    {
+        DES,
+        ThreeDES,
+        AES
+    }
+}
---
+++ StenoCryptor.Web/Models/DwmEmbedModel.cs
@@ -0,0 +1,19 @@
+﻿using StenoCryptor.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StenoCryptor.Web.Models
+{
+    public class DwmEmbedModel
+    {
+        public byte[] Container { get; set; }
+
+        public string Message { get; set; }
+
+        public EmbedType EmbedType { get; set; }
+
+        public CryptType CryptType { get; set; }
+    }
+}
