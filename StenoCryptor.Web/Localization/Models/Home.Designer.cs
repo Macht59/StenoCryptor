c8d199001ff45c4ec154c790ea438a835343da14
Igor Starodynov <igor.starodynov@outlook.com>
2013-04-26 14:55:56 +0300

    hz

--- StenoCryptor.Web/Localization/Models/Home.Designer.cs
+++ StenoCryptor.Web/Localization/Models/Home.Designer.cs
@@ -22,7 +22,7 @@ namespace StenoCryptor.Web.Localization.Models {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Home {
+    public class Home {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace StenoCryptor.Web.Localization.Models {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("StenoCryptor.Web.Localization.Models.Home", typeof(Home).Assembly);
@@ -51,12 +51,48 @@ namespace StenoCryptor.Web.Localization.Models {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Container:.
+        /// </summary>
+        public static string Container {
+            get {
+                return ResourceManager.GetString("Container", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Crypting Type:.
+        /// </summary>
+        public static string CryptingType {
+            get {
+                return ResourceManager.GetString("CryptingType", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Embeding Type:.
+        /// </summary>
+        public static string EmbedingType {
+            get {
+                return ResourceManager.GetString("EmbedingType", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Message:.
+        /// </summary>
+        public static string Message {
+            get {
+                return ResourceManager.GetString("Message", resourceCulture);
             }
         }
     }
--- StenoCryptor.Web/Models/DwmEmbedModel.cs
+++ StenoCryptor.Web/Models/DwmEmbedModel.cs
@@ -1,9 +1,5 @@
 ﻿using StenoCryptor.Commons;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Web;
 
 namespace StenoCryptor.Web.Models
 {
